0b5dc46b5fe7248b049a2f10bd08abfbfa09c165
Ethem <ethemkizilyer3546@gmail.com>
2022-09-08 01:58:56 +0300

    initialcommit

---
+++ app.js
@@ -0,0 +1,74 @@
+let kutu1 = document.querySelector("#kutu1");
+let kutu2 = document.querySelector("#kutu2");
+let ekle = document.querySelector("#ekle");
+let img = document.querySelector("img");
+
+let sol1 = 0,
+  sol2 = 700,
+  sag1 = 1260,
+  sag2 = 500;
+let soldur1, soldur2, sagdur1, sagdur2;
+
+
+function ileri1() {
+  sol1 += 1;
+  if (sol1 <= 600) {
+    kutu1.style.left = sol1 + "px";
+    kutu1.innerHTML = sol1 + "px";
+  } else {
+    setTimeout(ekle1, 1);
+    clearInterval(soldur1);
+    soldur2 = setInterval(ileri2, 10);
+  }
+}
+
+function ileri2() {
+  sol2 += 1;
+  if (sol2 <= 1260) {
+    kutu2.style.left = sol2 + "px";
+    kutu2.innerHTML = sol2 + "px";
+  } else {
+    clearInterval(soldur2);
+    sagdur1 = setInterval(geri1, 10);
+  }
+}
+function geri1() {
+  sag1 -= 1;
+  if (sag1 >= 700) {
+    kutu2.style.left = sag1 + "px";
+    kutu2.innerHTML = sag1 + "px";
+  } else {
+    clearInterval(sagdur1);
+    setTimeout(ekle1, 1);
+    sagdur2 = setInterval(geri2, 10);
+  }
+}
+function geri2() {
+  sag2 -= 1;
+  if (sag2 >= 0) {
+    kutu1.style.left = sag2 + "px";
+    kutu1.innerHTML = sag2 + "px";
+  } else {
+    clearInterval(sagdur2);
+    setTimeout(sıfırla, 1);
+  }
+}
+
+function sıfırla() {
+  (sol1 = 0), (sol2 = 700), (sag1 = 1200), (sag2 = 600);
+  soldur1 = setInterval(ileri1, 10);
+}
+
+soldur1 = setInterval(ileri1, 10);
+
+function ekle1() {
+  ekle.style.display = "block";
+  img.style.display = "block";
+
+
+
+  setInterval(function () {
+    ekle.style.display = "none";
+    img.style.display = "none";
+  }, 2000);
+}
